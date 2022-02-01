2eb47d707490a9c8c18f266a3bc9a1e73e6b874c
sunilreddy74 <sunilreddy74@yahoo.com>
2022-01-31 21:06:29 -0600

    Added validations to request object

--- TaxCalculator/Models/TaxRequest.cs
+++ TaxCalculator/Models/TaxRequest.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaxCalculator
 {
     [BindProperties(SupportsGet = true)]
     public class TaxRequest
     {
+        [Required]
         public string Area { get; set; }
 
+        [Required]
         public decimal Amount { get; set; }
         public string State { get; set; } = "NC";
     }
